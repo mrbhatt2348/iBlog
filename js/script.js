f6055ec222b61fe8b496fc1e39ecea650e1bd888
VIVEK SATI <viveksati2002@gmail.com>
2023-02-15 20:47:40 +0530

    adding testimonials

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
