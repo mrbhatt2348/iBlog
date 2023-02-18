2e9d2b25a8491e28c62638080d2f37569f366a68
Akshat Jain <84455686+AK-shat-JAIN@users.noreply.github.com>
2023-02-18 19:31:25 +0530

    Added landing page with Animated website name

---
+++ landing.js
@@ -0,0 +1,9 @@
+// const logo = document.querySelectorAll("#logo path");
+// for(let i=0;i<logo.length;i++){
+//     console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
+// }
+
+function redirectpage(){
+    window.location="index.html";
+}
+setTimeout('redirectpage()', 3500);
