cbd08cde7eda1c1a5aab8e8f37342bf912ca83a1
VIVEK SATI <viveksati2002@gmail.com>
2023-03-09 20:03:27 +0530

    Final commit

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-// main.js file
-var faq = document.getElementsByClassName("faq-page");
+var acc = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
-        }
-    });
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
