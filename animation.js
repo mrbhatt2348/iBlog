d40ee8d4ee1dfbaa63d036b4e2b9004ab27ebdab
Ritika Malik <ritikamalik100102@gmail.com>
2023-03-15 12:25:09 +0530

    entire website animation

---
+++ animation.js
@@ -0,0 +1,17 @@
+function reveall() {
+    var reveals = document.querySelectorAll(".reveall");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveall);
